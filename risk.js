86444158b72bfad76ad12304fd6f94cb22600a72
Andrew Cranston <andrewtcranston@gmail.com>
2016-02-17 18:09:36 -0800

    first commit - basic js object and static html page

---
+++ risk.js
@@ -0,0 +1,11 @@
+var RiskGame = {
+
+  // var title = 'RISK: The World is at War';
+  // var continentCount = 6;
+ 'title': 'RISK: The World is at War!',
+
+  getTitle: function(){
+    return this.title;
+  },
+  //return this;
+};
